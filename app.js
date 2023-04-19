def693a344f45170c127652b18e474ba6ef17406
ManiacalMaynard <maynard@maniacalindustries.com>
2023-04-19 11:02:23 -0700

    adds bricks to canvas

--- app.js
+++ app.js
@@ -6,6 +6,37 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
+//Let's talk bricks...
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+const bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+      const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = '#0095DD';
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+}
+
 //all the ball things:
 const ballRadius = 10;
 const drawBall = () => {
@@ -50,6 +81,7 @@ const drawPaddle = () => {
 
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
   drawPaddle();
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {

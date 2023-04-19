128b7fa7c3cc763b8a70fcc43c20515f98913690
ManiacalMaynard <maynard@maniacalindustries.com>
2023-04-19 10:15:07 -0700

    adds collision to ball

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = '#FF0000';
-ctx.fill();
-ctx.closePath();
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = 'green';
-ctx.fill();
-ctx.closePath();
+//all the ball things:
+const ballRadius = 10;
+const drawBall = () => {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+};
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
-ctx.stroke();
-ctx.closePath();
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x + dx > canvas.width - ballRadius || x + dx < ballRadius ? (dx = -dx) : dx;
+  y + dy > canvas.height - ballRadius || y + dy < ballRadius ? (dy = -dy) : dy;
+  x += dx;
+  y += dy;
+};
+
+setInterval(draw, 10);
 
 console.log("Don't push me cause I'm close to the edge...");

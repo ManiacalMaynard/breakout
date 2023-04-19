6e72cca2c882c78d10e6487f20f2cc42d7d5af6c
ManiacalMaynard <maynard@maniacalindustries.com>
2023-04-19 10:34:41 -0700

    adds interaction to paddle

--- app.js
+++ app.js
@@ -16,13 +16,51 @@ const drawBall = () => {
   ctx.closePath();
 };
 
+// User interaction defined here
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+function keyDownHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
+  }
+}
+function keyUpHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
+  }
+}
+
+// Here's the paddle stuff!
+const paddleHeight = 10;
+const paddleWidth = 75;
+let rightPressed = false;
+let leftPressed = false;
+let paddleX = (canvas.width - paddleWidth) / 2;
+const drawPaddle = () => {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = '#0095DD';
+  ctx.fill();
+  ctx.closePath();
+};
+
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   x + dx > canvas.width - ballRadius || x + dx < ballRadius ? (dx = -dx) : dx;
   y + dy > canvas.height - ballRadius || y + dy < ballRadius ? (dy = -dy) : dy;
   x += dx;
   y += dy;
+  if (rightPressed) {
+    paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+  } else if (leftPressed) {
+    paddleX = Math.max(paddleX - 7, 0);
+  }
 };
 
 setInterval(draw, 10);
